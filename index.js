a5ee6b5822aa1bc125753c244c4684b76019b1d4
Tammarra McPhaul <shaemc22@gmail.com>
2021-12-20 20:51:09 -0500

    commit tree d700633abca948a888dd3091734765f42352d880

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+let num3 = 24
+let num4 = 5
+const mod = num3 % num4;
+
+const max = Math.max(1,5,20,8,7);
